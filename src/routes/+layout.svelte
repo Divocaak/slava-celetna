75e74ef65d61b181edfee188548d73d09beed7fd
Vojtěch divoký <divokyvojta@gmail.com>
2024-09-22 14:20:37 +0200

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<slot></slot>
+
+<style>
+	:global(:root) {
+		--green: #8dcaa9;
+		--pink: #e795b6;
+		--white: #ffffff;
+
+		--py: 10vh;
+		--px: 5vw;
+	}
+
+	:global(html, div, body) {
+		margin: 0;
+		padding: 0;
+	}
+
+	:global(h1, h2, h3, p) {
+		color: var(--white);
+	}
+</style>
